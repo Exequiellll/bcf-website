8cbc24dbc74860c7c27c1c312f5b1f79262cf043
Exequiel Co <ejayrco@gmail.com>
2026-05-07 11:56:07 +0800

    fix authe login

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/js/app.js'])
     <style>
         body {
             font-family: 'Poppins', sans-serif;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,8 +45,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
     <!-- Vite CSS and JS -->
-    @viteReactRefresh
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/js/app.js'])
     
     <!-- Favicon -->
     <link rel="icon" type="image/png" href="{{ asset('images/bcf real logo.png') }}">
